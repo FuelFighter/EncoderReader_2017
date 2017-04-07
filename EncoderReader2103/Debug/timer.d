7c978df7cd9407366f6b21d4cb3ad9d4e1619399
Jørgen Jackwitz <jorgejac@ntnu.no>
2017-04-07 18:21:20 +0200

    Should work by counting the time between pulses. Pulse time to RPM works, but Pulse time is wrong.

--- EncoderReader2103/Debug/timer.d
+++ EncoderReader2103/Debug/timer.d
@@ -5,7 +5,7 @@ timer.d timer.o: ../../UniversalModuleDrivers/timer.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
- C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iocan32.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iocan128.h \
  C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iocanxx.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
@@ -27,7 +27,7 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
 
-C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iocan32.h:
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iocan128.h:
 
 C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iocanxx.h:
 
--- EncoderReader2103/main.c
+++ EncoderReader2103/main.c
@@ -16,31 +16,33 @@
 #include "../UniversalModuleDrivers/can.h"
 #include "../UniversalModuleDrivers/timer.h"
 
-#define COUNTCONSTANT 960000
+#define ENCODER_COUNT_CONSTANT 960000
+#define WHEEL_COUNT_CONSTANT 468750
 #define ENCODER_ID 5
 #define MEASURE 1
 #define SEND 2
+#define TIME_CONSTANT 1.126953
 
-#define FILTER_SHIFT 4
-#define ENABLE_DIFF1 PF3
-#define ENCODER_I_1	 PF0
-#define ENCODER_A_1	 PF1
-#define ENCODER_B_1  PF2
-#define ENABLE_DIFF2 PB3
-#define ENCODER_I_2	 PB0
-#define ENCODER_A_2	 PB1
-#define ENCODER_B_2  PB2
-#define WHEEL_PIN	 PB4	
+#define FILTER_SHIFT 2
+#define ENABLE_DIFF1 3
+#define ENCODER_I_1	 0
+#define ENCODER_A_1	 1
+#define ENCODER_B_1  2
+#define ENABLE_DIFF2 3
+#define ENCODER_I_2	 0
+#define ENCODER_A_2	 1
+#define ENCODER_B_2  2
+#define WHEEL_PIN	 4	
 
 static CanMessage_t txFrame;
 
 void pin_init(){
 	//Encoder 1 pin init
-	DDRE &= ~((1<<ENCODER_A_1)|(1<<ENCODER_B_1)|(1<<ENCODER_I_1));
+	DDRF &= ~((1<<ENCODER_A_1)|(1<<ENCODER_B_1)|(1<<ENCODER_I_1));
 	//Encoder 2 pin init
 	DDRB &= ~((1<<ENCODER_A_2)|(1<<ENCODER_B_2)|(1<<ENCODER_I_2));
 	//Enable Differential IC's
-	PORTE |= (1<<ENABLE_DIFF1);
+	PORTF |= (1<<ENABLE_DIFF1);
 	PORTB |= (1<<ENABLE_DIFF2);
 	//Wheel Hall Sensor pin init
 	// TBD
@@ -52,34 +54,63 @@ uint32_t w_rpm = 0;
 uint8_t m1_state = 0;
 uint8_t m2_state = 0;
 uint8_t w_state = 0;
-uint16_t m1_counts = 0;
-uint16_t m2_counts = 0;
-uint16_t w_counts = 0;
-uint16_t m1_samples = 0;
-uint16_t m2_samples = 0;
-uint16_t w_samples = 0;
+uint32_t m1_counts = 0;
+uint32_t m2_counts = 0;
+uint32_t w_counts = 0;
+uint32_t m1_temp_count = 0;
+uint32_t m2_temp_count = 0;
+uint32_t w_temp_count = 0;
 
-void low_pass_filter(uint16_t newValue, uint32_t *oldValue)
+void low_pass_filter(uint32_t newValue, uint32_t *oldValue)
 {
 	 *oldValue = ((*oldValue - (*oldValue >> FILTER_SHIFT) + newValue) >> FILTER_SHIFT);
 }
 
-uint16_t calculate_rpm(uint16_t count)
+uint16_t calculate_rpm_motors(uint32_t count)
 {
 	uint32_t output = 0;
 	if (count >= 200)
 	{
-		output = COUNTCONSTANT/count;
-	} else if (count == 65536)
+		output = ENCODER_COUNT_CONSTANT/count;
+	} else if (count > ENCODER_COUNT_CONSTANT)
 	{
 		output = 0;
+	} else 
+	{
+		output = 0xFFFF;	
 	}
+	
+	return output;
+}
+
+uint16_t calculate_rpm_wheel(uint32_t count)
+{
+	uint32_t output = 0;
+	if (count >= 200)
+	{
+		output = WHEEL_COUNT_CONSTANT/count;
+	} else if (count > WHEEL_COUNT_CONSTANT)
+	{
+		output = 0;
+	} else 
+	{
+		output = 0xFFFF;	
+	}
+	
 	return output;
 }
 
 void timers_conf()
 {
-
+	//Enabling Timer 1 with no prescaling and overflow interrupt
+	TCCR1B |= (1<<CS10);
+	TIMSK1 |= (1<<TOIE1);
+	//Enabling Timer 3 with no prescaling and overflow interrupt
+	TCCR3B |= (1<<CS30);
+	TIMSK3 |= (1<<TOIE3);
+	//Enabling Timer 2 with 1024 prescaling and overflow interrupt
+	TCCR2A |= (1<<CS20)|(1<<CS22);
+	TIMSK2 |= (1<<TOIE2);
 }
 
 int main(void)
@@ -96,9 +127,11 @@ int main(void)
 	uint8_t state = MEASURE;
     txFrame.id = ENCODER_ID;
 	txFrame.length = 5;
+	uint16_t time = 0;
 	
 	while (1) 
     {
+		
 		if (timer_elapsed_ms(TIMER0) > 100)
 		{
 			state = SEND;
@@ -107,50 +140,47 @@ int main(void)
 		switch (state)
 		{
 		case SEND:
-			m1_counts = m1_counts/m1_samples;
-			low_pass_filter(calculate_rpm(m1_counts),&m1_rpm);
+			cli();
+			
+			m1_rpm = calculate_rpm_motors(m1_counts);
 			txFrame.data[0] = m1_rpm >> 8;
 			txFrame.data[1] = m1_rpm;
-			m1_samples = 0;
+			printf("RPM: %u\n",m1_rpm);
 			m1_counts = 0;
 			
-			m2_counts = m2_counts/m2_samples;
-			low_pass_filter(calculate_rpm(m2_counts),&m2_rpm);
+			m2_rpm = calculate_rpm_motors(m2_counts);
 			txFrame.data[2] = m2_rpm >> 8;
 			txFrame.data[3] = m2_rpm;
-			m2_samples = 0;
-			m2_counts = 0;
 			
-			w_counts = w_counts/w_samples;
-			low_pass_filter(calculate_rpm(w_counts),&w_rpm);
-			txFrame.data[4] = w_rpm; 
-			w_samples = 0;
-			w_counts = 0;
+			w_rpm = calculate_rpm_wheel(w_counts);
+			txFrame.data[4] = w_rpm;
 			
 			can_send_message(&txFrame);
 			state = MEASURE;
 			timer_start(TIMER0);
+			
+			sei();
 			break;
 			
 		case MEASURE:
 			//Encoder 1
-			if ((PINB & (1<<ENCODER_I_1)) && !m1_state)
+			if (!(PINF&(1<<ENCODER_I_1)) && (m1_state == 0))
 			{
-				m1_counts += TCNT1;
+				m1_temp_count = TCNT1;
 				TCNT1 = 0;
-				m1_samples++;
+				low_pass_filter(m1_temp_count,&m1_counts);
 				m1_state = 1;
-			} else if (!(PINB & (1<<ENCODER_I_1)) && m1_state)
+			} else if ((PINF &(1<<ENCODER_I_1)) && m1_state)
 			{
 				m1_state = 0;
 			}
 			
 			//Encoder 2
-			if ((PINF & (1<<ENCODER_I_2)) && !m1_state)
+			if ((PINB & (1<<ENCODER_I_2)) && !m1_state)
 			{
-				m2_counts = TCNT3;
+				m2_temp_count = TCNT3;
 				TCNT3 = 0;
-				m2_samples++;
+				low_pass_filter(m2_temp_count,&m2_counts);
 				m2_state = 1;
 			} else if (!(PINB & (1<<ENCODER_I_1)) && m1_state)
 			{
@@ -162,16 +192,31 @@ int main(void)
 			{
 				w_counts = TCNT2;
 				TCNT2 = 0;
-				w_samples++;
+				low_pass_filter(w_temp_count,&w_counts);
 				w_state = 1;
 			} else if (!(PINB & (1<<WHEEL_PIN)) && m1_state)
 			{
 				w_state = 0;
 			}
-			
 			break;
 		}
 	}
 }
 
+ISR(TIMER1_OVF_vect)
+{
+	m1_temp_count += 0xFFFF;
+	TCNT1 = 0;
+}
 
+ISR(TIMER2_OVF_vect)
+{
+	w_temp_count += 0xFF;
+	TCNT2 = 0;
+}
+
+ISR(TIMER3_OVF_vect)
+{
+	m2_temp_count += 0xFFFF;
+	TCNT3 = 0;
+}
